4486b2faee94a0e764741da0b6dc07009b9807e6
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-07-10 22:49:24 +0530

    up

---
+++ src/Frontend/Web/TS/code/z01 7/index.tsx
@@ -0,0 +1,16 @@
+import { Title, H1, Syntax, Para ,HL,List,
+,} from "../../../../../Backend/UI";
+
+const T7 = () => {
+  return (
+    <>
+      <Title></Title>
+
+    </>
+  );
+};
+
+export default 7;
+
+const app = ``;
+const style = ``;
