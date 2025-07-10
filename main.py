e3eddd7f3aa5102368742c90f7b7438ad3845c27
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-07-10 10:09:11 +0530

    updated

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
 
-def rename_files_in_folder(folder_path, prefix=""):
+def rename_files_in_folder(folder_path, prefix="typescript-slides_page"):
     files = os.listdir(folder_path)
     files = [f for f in files if os.path.isfile(os.path.join(folder_path, f))]
 
     for index, filename in enumerate(files, start=1):
         name, ext = os.path.splitext(filename)
-        new_name = f"{prefix}{index}{ext}"
+        new_name = f"{prefix}-{index}{ext}"
         old_path = os.path.join(folder_path, filename)
         new_path = os.path.join(folder_path, new_name)
         os.rename(old_path, new_path)
@@ -15,7 +15,7 @@ def rename_files_in_folder(folder_path, prefix=""):
 
 
 # 📂 Replace this with your folder path
-folder_path = r"D:\course\Web Development\tut-web-react-js-mu\temp\TEMP"
+folder_path = r"D:\course\Web Development\tut-web-ts-mu\slides"
 rename_files_in_folder(folder_path)
 
 # files = [f for f in files if f.endswith(".jpg")]
